b72e36d5876cad4d2afd612c0e9d954bfe517b61
Zukicode <zukicode@gmail.com>
2023-01-17 20:22:32 +0200

    upload application

---
+++ src/components/Result/Result.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Result = ({ value }) => {
+	return (
+		<div className='ouptop'>
+			<textarea type='text' value={value} placeholder='0' />
+		</div>
+	);
+};
+
+export default Result;
